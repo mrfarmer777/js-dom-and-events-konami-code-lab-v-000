7e7df372a3798a7b54e18cc69d5d3a082bda35d5
Matt Farmer <mrfarmer777@gmail.com>
2018-07-28 20:45:00 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown',keyHandler);
+}
 
+
+let prog=0;
+function keyHandler(e){
+  console.log("key: ",e.which);
+  console.log("Prog: ", prog);
+  if(e.which===code[prog] || e.detail===code[prog]){
+    prog++;
+  } else {
+    prog=0;
+  }
+  if(prog===10){
+    alert("Hurray!");
+  }
 }
